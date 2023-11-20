6f1b79647898ffe856d773e62155f193458aec6b
Paul Ehrling <ghostlest330@gmail.com>
2023-11-20 22:07:20 +0100

    Router

--- src/app/routes/BugReportRoute.ts
+++ src/app/routes/BugReportRoute.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 
+import AuthenticateToken from '../middleware/AuthenticateToken';
+import AuthenticateRole from '../middleware/AuthenticateRole';
+
 const router = express.Router();
 
 export default router
