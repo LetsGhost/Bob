de1799a841c02d6479df4ed83a9d18e95c27b103
Paul Ehrling <ghostlest330@gmail.com>
2023-11-23 12:01:28 +0100

    Auth

--- src/app/middleware/AuthenticateToken.ts
+++ src/app/middleware/AuthenticateToken.ts
@@ -4,24 +4,19 @@ import { Request, Response, NextFunction } from 'express';
 class AuthenticateToken{
     async authenticateToken(req: Request, res: Response, next: NextFunction) {
         try {
-            let token = req.headers.token; // Assuming you set the token as 'token' cookie
+            const token = req.cookies.token; // Assuming you set the token as 'token' cookie
 
             if (!token) {
                 return res.status(401).json({ message: 'Token not provided' });
             }
-
-            // Ensure token is a string
-            if (Array.isArray(token)) {
-                token = token[0]; // or token.join('') depending on your use case
-            }
-            const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET!) as any as { userId: string, userRole: string };
+            
+            const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET!) as { userId: string, userRole: string };
 
             req.params.userId = decodedToken.userId;
             req.params.userRole = decodedToken.userRole;
 
             next();
         } catch (error) {
-            console.log("Error authenticating token: ", error)
             return res.status(401).json({ message: 'Invalid token' });
         }
     };
