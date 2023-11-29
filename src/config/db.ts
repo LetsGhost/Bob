449972d0f01e35cd023057c7cdc585a10e9effdd
Paul Ehrling <ghostlest330@gmail.com>
2023-11-29 16:54:50 +0100

    Add db stats

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import {redisClient} from './redis';
 
 const connectToDatabase = async () => {
     try {
@@ -15,4 +16,22 @@ const connectToDatabase = async () => {
     }
 };
 
-export { connectToDatabase };
+const logDatabaseStats = async () => {
+    mongoose.connection.on('connected', async () => {
+        try {
+            const stats = await mongoose.connection.db.stats();
+
+            if (!redisClient) {
+                console.error('Redis client is not connected.');
+                return;
+            }
+
+            // Store the stats in Redis
+            redisClient.set('dbStats', JSON.stringify(stats));
+        } catch (err) {
+            console.error(err);
+        }
+    });
+};
+
+export { connectToDatabase, logDatabaseStats };
