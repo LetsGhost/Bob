1e975d3c8001bd1e39823777dbf2d4c71d73ea23
Paul Ehrling <ghostlest330@gmail.com>
2023-11-30 10:43:24 +0100

    Add get Ip

---
+++ src/app/Utils/ipUtils.ts
@@ -0,0 +1,14 @@
+import { Request } from 'express';
+import logger from '../../config/winstonLogger';
+
+export function getClientIp(req: Request): string | undefined {
+  try{
+    const forwarded = req.headers['x-forwarded-for'];
+    let ip = Array.isArray(forwarded) ? forwarded[0] : forwarded;
+    return ip || req.socket.remoteAddress;
+  } catch(error){
+    logger.error('Error getting client ip:', error, {service: 'getClientIp'});
+  }
+}
+
+export default getClientIp
