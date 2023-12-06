ceaf75eb7cb22797afbf01595311e23f91b775d8
Paul Ehrling <ghostlest330@gmail.com>
2023-12-06 17:22:47 +0100

    Change redis connection string

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -6,8 +6,8 @@ let redisClient: Redis | null = null;
 const connectToRedis = async (): Promise<Redis | null> => {
 
   try {
-    const redisUrl = `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || '6379'}`;
-    logger.info(`Connecting to Redis at ${redisUrl}`, { service: 'connectToRedis' });
+    const redisUrl = `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+    logger.info(`Connecting to Redis at ${process.env.REDIS_PORT}`, { service: 'connectToRedis' });
 
     redisClient = new Redis(redisUrl);
   } catch (err) {
